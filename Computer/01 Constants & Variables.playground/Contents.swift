d927aaf86a87742c400e5a414e3fac53517b4669
Gwangyu Lee <fred.lee.9808@icloud.com>
2024-03-12 13:01:47 +0900

    Add file via upload

---
+++ Computer/01 Constants & Variables.playground/Contents.swift
@@ -0,0 +1,99 @@
+import UIKit
+
+/*:
+ # Constant
+ 
+ ## Syntax
+ ```
+ let maxNumber = 1
+ ```
+ */
+
+
+
+/*:
+ # Variable
+ 
+ ## Syntax
+ ```
+ var currentNumber = 1
+ ```
+ */
+
+
+
+/*:
+ # Multiple Variables
+ 
+ ## Syntax
+ ```
+ var x = 1, y = 2, z = 3
+ ```
+ */
+
+
+
+/*:
+ # Type Inference
+ 
+ ## Syntax
+ ```
+ print(type(of: x))
+ ```
+ */
+
+
+
+/*:
+ # Type Annotations
+ 
+ ## Syntax
+ ```
+ var firstAlphabet: Character = "A"
+ ```
+ */
+
+
+
+/*:
+ # Multiple Type Annotations
+ 
+ ## Syntax
+ ```
+ var red, green, blue: Double
+ ```
+ */
+
+
+
+/*:
+ # Boolean
+ 
+ ## Syntax
+ ```
+ var valid = true
+ ```
+ */
+
+
+
+/*:
+ # Tuple
+ 
+ ## Syntax
+ ```
+ var lecture = ("marte", 202)
+ ```
+ */
+
+
+
+/*:
+ # String Interpolation
+ 
+ ## Syntax
+ ```
+ var labName = "\(lab) lab"
+ ```
+ */
+
